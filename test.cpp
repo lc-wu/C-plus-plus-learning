d71bf4d2d5484d53cf6851a25ec98357f5c56935
wulc <wulc1994@outlook.com>
2020-05-01 21:39:27 +0800

    2020.5.1

--- test.cpp
+++ test.cpp
@@ -1,21 +1,18 @@
-#include<iostream>
-#include<list>
-
+//使用assign进行元素重置
+#include <iostream>
+#include <list>
 using namespace std;
-
+  
 int main()
 {
-    list<int> a;
-    list<int> b;
-    a.push_back(5);
-    a.push_back(4);
-    b = a;
-    list<int>::iterator it = a.begin();
-    a.insert(it, b.begin(), b.end());
-    int c;
-    c = a.pop_front();
-    cout << c << endl;
-    for (it = b.begin(); it != b.end(); it++)
+    list<double> list_One,list_Two,list_Three;
+    for(int i=0;i<10;++i){
+        list_One.push_front(i);
+    }
+    list_Two = list_One;
+    //list_Two.assign(5,5.6);  //将list_Two的值设置为5个5.6
+    list_Three.assign(list_One.begin(),list_One.end());
+    for (list<double>::iterator it = list_Two.begin(); it != list_Two.end(); it++)
         cout << *it << endl;
     return 0;
 }
